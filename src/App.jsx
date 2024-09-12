29b340e6a4b438b5675d9ce2d3fef82a9d72cd1d
peemtanapat <pm.tanapat@gmail.com>
2024-09-12 16:38:51 +0700

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import InfoSection from "./components/InfoSection";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <InfoSection />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div id="header">
+      <img src="investment-calculator-logo.png" alt="" />
+      <h1>Investment Calculator</h1>
+    </div>
+  );
+}
