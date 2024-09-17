dc7a31d9bd3c4fb32f828c3942332f9cfff31c5f
TanapatC <TanapatC@generali.co.th>
2024-09-17 15:12:02 +0700

    Revamp NumberInput formatter

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,12 +1,34 @@
-export default function UserInput({ label, handleOnChange, defaultValue = 0 }) {
+import { useState } from "react";
+
+export default function UserInput({ label, handleOnChange }) {
+  const [value, setValue] = useState(0)
+
+  function handleNumber(e) {
+    const rawValue = e.target.value;
+    const formatted = format(rawValue)
+
+    setValue(formatted)
+
+    const deFormatted = deFormat(formatted)
+
+    handleOnChange(parseInt(deFormatted))
+  }
+
+  function format(input) {
+    return parseInt(deFormat(input)).toLocaleString()
+  }
+
+  function deFormat(input) {
+    return input.replaceAll(',', '')
+  }
+
   return (
     <div id="user-input">
       <label>{label}</label>
       <input
-        type="number"
-        min={0}
-        defaultValue={defaultValue}
-        onChange={(e) => handleOnChange(parseInt(e.target.value))}
+        type="text"
+        value={value}
+        onChange={handleNumber}
       />
     </div>
   );
