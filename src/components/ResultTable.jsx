99f7dda54472f6b3d41f1b8ee1a14ef5561a12a6
peemtanapat <pm.tanapat@gmail.com>
2024-09-21 12:25:33 +0700

    Use table instead div

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -14,31 +14,29 @@ export default function ResultTable({
   });
 
   return (
-    <div id="result">
-      <table>
-        <thead>
-          <tr>
-            <th style={{ width: "50px" }}>Year</th>
-            <th style={{ width: "150px" }}>Investment Value</th>
-            <th style={{ width: "150px" }}>Interest (Year)</th>
-            <th style={{ width: "150px" }}>Total Interest</th>
-            <th style={{ width: "150px" }}>Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          {annualData.map((row) => {
-            return (
-              <tr key={row.year}>
-                <td>{row.year}</td>
-                <td>{row.annualInvestment}</td>
-                <td>{row.interest}</td>
-                <td>{row.interest}</td>
-                <td>{row.valueEndOfYear}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <table id="result">
+      <thead>
+        <tr>
+          <th style={{ width: "50px" }}>Year</th>
+          <th style={{ width: "150px" }}>Investment Value</th>
+          <th style={{ width: "150px" }}>Interest (Year)</th>
+          <th style={{ width: "150px" }}>Total Interest</th>
+          <th style={{ width: "150px" }}>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        {annualData.map((row) => {
+          return (
+            <tr key={row.year}>
+              <td>{row.year}</td>
+              <td>{row.annualInvestment}</td>
+              <td>{row.interest}</td>
+              <td>{row.interest}</td>
+              <td>{row.valueEndOfYear}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
